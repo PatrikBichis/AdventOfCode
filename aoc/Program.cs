48156780bd4952f048cf96f51620b30c1975eff2
Patrik Olsson Bichis <patrik.olsson@x-firm.com>
2023-12-04 20:15:30 +0100

    Fixes

--- aoc/Program.cs
+++ aoc/Program.cs
@@ -15,10 +15,10 @@ namespace aoc2020
             //new aoc23.Puzzles._2022.Day1_2(InputType.TestInput).Run().Print();
             //new aoc23.Puzzles._2022.Day1_2(InputType.Input).Run().Print();
 
-            //new aoc23.Puzzles._2023.Day4_1(InputType.TestInput).Run().Print();
-            new aoc23.Puzzles._2023.Day3_1(InputType.Input).Run().Print();
-            //new aoc23.Puzzles._2023.Day4_2(InputType.TestInput).Run().Print();
-            new aoc23.Puzzles._2023.Day4_2(InputType.Input).Run().Print();
+            //new aoc23.Puzzles._2023.Day5_1(InputType.TestInput).Run().Print();
+            //new aoc23.Puzzles._2023.Day5_1(InputType.Input).Run().Print();
+            //new aoc23.Puzzles._2023.Day5_2(InputType.TestInput).Run().Print();
+            //new aoc23.Puzzles._2023.Day5_2(InputType.Input).Run().Print();
 
             Console.WriteLine(Environment.NewLine);
             Console.WriteLine("---------------------------");
