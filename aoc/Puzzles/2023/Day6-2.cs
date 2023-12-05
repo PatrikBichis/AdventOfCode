ba2df45e5d5300b1e56682cb486909f879ae59ca
Patrik Olsson Bichis <Patrik.OlssonBichis@meritor.com>
2023-12-05 22:16:11 +0100

    Fixes

---
+++ aoc/Puzzles/2023/Day6-2.cs
@@ -0,0 +1,44 @@
+﻿using aoc.Helpers;
+using aoc2020;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Metrics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc23.Puzzles._2023
+{
+    internal class Day6_2 : PuzzelBase, IPuzzel
+    {
+        public Day6_2(InputType test) : base(test, 6, year: 2023)
+        {
+
+        }
+
+        public IPuzzel Run()
+        {
+
+            try
+            {
+
+                for (var i = 0; i < Input.Length; i++)
+                {
+                    var input = Input[i];
+
+                }
+
+                Answer = "N/A";
+            }
+            catch (Exception ex)
+            {
+                Answer = "Error: " + ex.Message;
+                return this;
+            }
+
+            return this;
+        }
+    }
+}
