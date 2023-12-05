7c88cdbf1920fab6859c75261d38daa9fa031c92
Patrik Olsson Bichis <patrik.olsson@x-firm.com>
2023-12-05 06:43:54 +0100

    Started

--- aoc/Puzzles/2023/Day5-1.cs
+++ aoc/Puzzles/2023/Day5-1.cs
@@ -1,7 +1,9 @@
-﻿using aoc2020;
+﻿using aoc.Helpers;
+using aoc2020;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,14 +19,49 @@ namespace aoc23.Puzzles._2023
 
         public IPuzzel Run()
         {
+
+            var seedsValues = new List<int>();
+            var seedToSoilMap = new List<MapItem>();
+            var soilToFertilizerMap = new List<MapItem>();
+            var fertilizerToWaterMap = new List<MapItem>();
+            var waterToLightMap = new List<MapItem>();
+            var lightToTemperatureMap = new List<MapItem>();
+            var temperatureToHumidityMap = new List<MapItem>();
+            var humidityToLocationMap = new List<MapItem>();
+            var seeds = new List<Seed>();
             
             try
             {
+                var lastCat = "";
+
                 for (var i = 0; i < Input.Length; i++)
                 {
-                    
+                    var input = Input[i];
+
+                    if (input.Before(':').Trim() == "seeds" && lastCat == "") {
+                        seedsValues = input.After(':').ValuesSeparatedBy(' ');
+                    }
+                    else if (input.Before(':').Trim() == "soil-to-fertilizer map" || lastCat == "soil-to-fertilizer map")
+                    {
+                        lastCat = "soil-to-fertilizer map";
+                        var mapValues = input.ValuesSeparatedBy(' ');
+
+                        var destinationRangeStart = mapValues[0];
+                        var sourceRangeStart = mapValues[1];
+                        var RangeLength = mapValues[2];
+
+                        for(int j = 0; j < RangeLength; j++)
+                        {
+                            seedToSoilMap.Add(new MapItem { source = destinationRangeStart + j, target = sourceRangeStart + j });
+                        }
+                    }
                 }
 
+                seedsValues.ForEach(value => seeds.Add(new Seed(value)));
+
+                seeds.ForEach(seed => seed.ExtractDataFromMaps(
+                    seedToSoilMap)
+                );
 
                 Answer = "N/A";
             }
@@ -35,6 +72,47 @@ namespace aoc23.Puzzles._2023
             }
 
             return this;
+        }
+
+        public class MapItem
+        {
+            public int source = 0;
+
+            public int target = 0;
+        }
+
+        public class Seed
+        {
+            public int SeedValue = 0;
+
+            public int Soil = 0;
+
+            public int Fertilizer = 0;
+
+            public int Water = 0;
+
+            public int Light = 0;
+
+            public int Temperature = 0;
+
+            public int Humidity = 0;
+
+            public int Location = 0;
+
+            public Seed(int value)
+            {
+                SeedValue = value;
+            }
+
+
+            public void ExtractDataFromMaps(
+                List<MapItem> seedToSoilMap
+            )
+            {
+                var soil = seedToSoilMap.FirstOrDefault(x => x.source == SeedValue);
+                if (soil == null) Soil = soil.target;
+                else Soil = SeedValue;
+            }
         }
     }
 }
